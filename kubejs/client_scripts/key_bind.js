a3ce566ddce15fdd38aa337b17e7c443327208e6
oqXoXpo <694164189@qq.com>
2024-04-16 16:50:35 +0800

    初始化

--- kubejs/client_scripts/key_bind.js
+++
@@ -1,7 +0,0 @@
-const $KeyMapping = Java.loadClass('net.minecraft.client.KeyMapping')
-
-ClientEvents.tick(event => {
-    if (global.OrganSkill.consumeClick()) {
-        event.player.sendData('ogran_key_pressed')
-    }
-})
