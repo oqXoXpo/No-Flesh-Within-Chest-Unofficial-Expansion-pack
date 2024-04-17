c8e0154499fc481495a302999e683d7c3cbf9268
oqXoXpo <694164189@qq.com>
2024-04-17 16:20:08 +0800

    新建创造物品栏

--- kubejs/startup_scripts/modlc/creative_tab_register.js
+++ kubejs/startup_scripts/modlc/creative_tab_register.js
@@ -1,4 +1,4 @@
 //新建创造物品栏
-const $CreativeTabRegistry1 = Java.loadClass("dev.architectury.registry.CreativeTabRegistry")
+const $CreativeTabRegistrymore = Java.loadClass("dev.architectury.registry.CreativeTabRegistry")
 
-$CreativeTabRegistry1.create(Utils.id("kubejs:moreorgans"), () => Item.of("kubejs:burning_heart"))
+$CreativeTabRegistrymore.create(Utils.id("kubejs:moreorgans"), () => Item.of("kubejs:burning_heart"))
